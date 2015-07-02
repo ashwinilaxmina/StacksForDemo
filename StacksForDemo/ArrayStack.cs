9b5b53bccd6c7a1035d2ad26ea648a3552f37c30
Ashwini Laxminarayana <ashwini.laxminarayana@aviva.co.uk>
2015-07-02 13:36:26 +0100

    Array stack - needs further work and refactoring

---
+++ StacksForDemo/ArrayStack.cs
@@ -0,0 +1,34 @@
+﻿
+namespace StacksForDemo
+{
+    public class ArrayStack
+    {
+        private int[] stackArray;
+
+        private int stackSize;
+
+        private int count;
+
+        public ArrayStack(int size)
+        {
+            stackSize = size;
+            stackArray = new int[size];
+        }
+
+        public void Push(int item)
+        {
+            stackArray[count] = item;
+            count++;
+        }
+
+        public int Pop()
+        {
+            return stackArray[--count];
+        }
+
+        public int Peek()
+        {
+            return stackArray[count - 1];
+        }
+    }
+}
--- StacksForDemo/Program.cs
+++ StacksForDemo/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StacksForDemo
+﻿namespace StacksForDemo
 {
-    class Program
+    using System;
+
+    public class Program
     {
         static void Main(string[] args)
         {
+            ArrayStack integerStack = new ArrayStack(10);
+
+            integerStack.Push(2);
+            integerStack.Push(4);
+            Console.WriteLine(integerStack.Peek());
+            Console.WriteLine(integerStack.Pop());
+            Console.WriteLine(integerStack.Pop());
+            Console.ReadLine();
         }
     }
 }
